960ca303c15c577adc695ab6f43a948b5cfdfaae
Caique Caleiro <caiquecaleiro@gmail.com>
2017-01-15 19:23:43 -0200

    Add shared module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { RecipesComponent } from './recipes.component';
 import { RecipeStartComponent } from './recipe-start.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item.component';
@@ -20,7 +20,7 @@ import { recipesRouting } from './recipes.routing';
     RecipeDetailComponent
   ],
   imports: [
-    CommonModule, 
+    SharedModule, 
     ReactiveFormsModule,
     recipesRouting
   ]
